cf27d1ff70dac99af6b2e9f525e7c9a3461ce020
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-12-31 09:48:39 +0100

    Creer une interface personneInt

---
+++ interface_heritage/personneInt.java
@@ -0,0 +1,8 @@
+package interface_heritage;
+
+public interface personneInt {
+    void Afficher();
+    void setNom(String nom);
+    void setPrenom(String prenom);
+    void setAdresse(String adresse);
+}
