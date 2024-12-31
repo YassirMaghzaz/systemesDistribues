4c95dbaa9cbd9ad8bbfa5c7154051a4eb631f6f9
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-12-31 10:06:06 +0100

    creer une classe Employe implementant employeInt

---
+++ interface_heritage/Employe.java
@@ -0,0 +1,22 @@
+package interface_heritage;
+
+public class Employe extends personne implements EmployeInt {
+    private String fonction;
+    private double salaire;
+
+    @Override
+    public void setFonction(String f) {
+        this.fonction = f;
+    }
+
+    @Override
+    public void setSalaire(double s) {
+        this.salaire = s;
+    }
+
+    @Override
+    public void Afficher() {
+        super.Afficher();
+        System.out.println("Fonction: "+ fonction+ ", Salaire: "+ salaire);
+    }
+}
