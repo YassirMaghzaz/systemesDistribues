c7b0db4408c9e5956097b194edfdc790a4ddd367
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 15:11:06 +0100

    Add Lecteur and Tableau classes for thread-based reading demonstration

---
+++ gestion_threads/variable_partage/Lecteur.java
@@ -0,0 +1,32 @@
+package gestion_threads.variable_partage;
+
+public class Lecteur extends Thread {
+
+    private Tableau tab;
+    
+    public Lecteur(Tableau T){
+        tab = T;
+    }
+
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            tab.Lire();
+        }
+    }
+
+    public static void main(String[] args) {
+       int tabEntiers[] = new int[10];
+       for (int i = 0; i < 10; i++) {
+           tabEntiers[i] = i;
+       }
+
+       Tableau T = new Tableau(tabEntiers);
+
+       Thread L1 = new Lecteur(T);
+       Thread L2 = new Lecteur(T);
+       
+       L1.start();
+       L2.start();
+    
+    }
+}
---
+++ gestion_threads/variable_partage/Tableau.java
@@ -0,0 +1,16 @@
+package gestion_threads.variable_partage;
+
+public class Tableau {
+    int tab[];
+    int indice;
+    
+    public Tableau(int[] T){
+        tab = T;
+        indice = 0;
+    }
+
+    public void Lire() {
+        System.out.println("Lu : " + tab[indice]);
+        indice++;
+    }
+}
