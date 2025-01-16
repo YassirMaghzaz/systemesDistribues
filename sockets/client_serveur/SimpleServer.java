d583272aa25c14f571a7ac2a4b108d23019ed96a
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 17:12:06 +0100

    Add SimpleServer class to implement a basic server for client-server communication

---
+++ sockets/client_serveur/SimpleServer.java
@@ -0,0 +1,25 @@
+package sockets.client_serveur;
+
+import java.io.*;
+import java.net.*;
+
+public class SimpleServer {
+    private static final int PORT = 9876;
+    
+    public static void main (String[] args) throws IOException {
+        try (ServerSocket SS = new ServerSocket(PORT)) {
+            System.out.println("Server is running on port " + PORT);
+            while(true) {
+                try (Socket s = SS.accept();
+                BufferedReader in = new BufferedReader( new InputStreamReader(s.getInputStream()));
+                PrintWriter out = new PrintWriter(s.getOutputStream(), true)) {
+                    String message = in.readLine();
+                    System.out.println("Message reçu : "+ message);
+                    out.println("Message reçu par le serveur : "+ message);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur du serveur: " + e.getMessage());
+        }
+    }
+}
