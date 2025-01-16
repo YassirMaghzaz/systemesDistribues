37be391e61898e7e5dd56600ddc18dbdbd9cd6e4
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 22:34:12 +0100

    Refactor Server2 to bind Servant1 and Servant2 to the registry and update Servant3 instantiation

--- Examen/RMI/Server2.java
+++ Examen/RMI/Server2.java
@@ -9,12 +9,17 @@ public class Server2 {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.createRegistry(1099);
+           
+            Servant1 servant1 = new Servant1();
+            registry.rebind("Servant1", servant1);
 
-            Servant1Interface servant1 = (Servant1Interface) registry.lookup("Servant1");
-            Servant2Interface servant2 = (Servant2Interface) registry.lookup("Servant2");
+            Servant2 servant2 = new Servant2();
+            registry.rebind("Servant2", servant2);
+            
+            Servant1Interface servantAssister1 = (Servant1Interface) registry.lookup("Servant1");
+            Servant2Interface servantAssister2 = (Servant2Interface) registry.lookup("Servant2");
 
-            Servant3 servant3 = new Servant3(servant1, servant2);
-
+            Servant3 servant3 = new Servant3(servantAssister1, servantAssister2);
             registry.rebind("Servant3", servant3);
 
             System.out.println("Serveur S2 en cours d'exécution...");
