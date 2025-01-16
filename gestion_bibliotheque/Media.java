96063563ff33bd2adb65734f5f3a1f66115e4be9
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 13:36:37 +0100

    Rendre les attributs de la classe Media protégés et ajouter un constructeur pour initialiser les propriétés

--- gestion_bibliotheque/Media.java
+++ gestion_bibliotheque/Media.java
@@ -1,7 +1,15 @@
 package gestion_bibliotheque;
 
 public abstract class  Media {
-    String title;
-    int year;
-    boolean borrowed;
+    protected String title;
+    protected int year;
+    protected boolean borrowed;
+
+    public Media(String title, int year) {
+        this.title = title;
+        this.year = year;
+        this.borrowed = false;
+    }
+
+    public abstract void displayInfo();
 }
