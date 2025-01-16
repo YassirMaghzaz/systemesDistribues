e0b45667696bea5a4a43bb8e0a4f4a974faf64c6
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 13:41:35 +0100

    Modifier la classe Magazine pour étendre Media, rendre issueNumer privé et ajouter un constructeur pour l'initialisation

--- gestion_bibliotheque/Magazine.java
+++ gestion_bibliotheque/Magazine.java
@@ -1,9 +1,20 @@
 package gestion_bibliotheque;
 
-public class Magazine extends Book implements Borrowable {
-    int issueNumer;
+public class Magazine extends Media implements Borrowable {
+    private int issueNumer;
     
+    public Magazine(String title, int year, int issueNumber) {
+        super(title, year);
+        this.issueNumer = issueNumber;
+    }
+
+    @Override
     public void borrow() {
         this.borrowed = true;
+    }
+    
+    @Override
+    public void displayInfo() {
+        System.out.println("Magazine: " + this.title + " (" + this.year + ") #" + this.issueNumer);
     }
 }
