80880f09341f9baca1989ba9204fa9dcf684aa95
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 13:39:01 +0100

    Rendre l'attribut author de la classe Book privé et ajouter un constructeur pour initialiser les propriétés

--- gestion_bibliotheque/Book.java
+++ gestion_bibliotheque/Book.java
@@ -1,9 +1,20 @@
 package gestion_bibliotheque;
 
 public class Book extends Media implements Borrowable {
-    String author;
+    private String author;
 
+    public Book(String title, int year, String author) {
+        super(title, year);
+        this.author = author;
+    }
+
+    @Override
     public void borrow() {
         this.borrowed = true;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: " + this.title + " (" + this.year + ") by " + this.author);
     }
 }
