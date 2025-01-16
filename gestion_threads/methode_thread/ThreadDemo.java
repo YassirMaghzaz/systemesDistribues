a5fa3927b51b771a86c32f09aa666952fd6e46a2
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 14:26:07 +0100

    Enhance ThreadDemo to include thread status messages and handle interruptions

--- gestion_threads/methode_thread/ThreadDemo.java
+++ gestion_threads/methode_thread/ThreadDemo.java
@@ -3,9 +3,14 @@ package gestion_threads.methode_thread;
 public class ThreadDemo {
     public static void main(String[] args) {    
         ThreadExample t1 = new ThreadExample("Hello");
+        System.out.println("Starting The Main Thread...");
         t1.start();
-        while (t1.isAlive()) {
-            System.out.println("Thread 1 is still alive");
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread has been interrupted : " + e.getMessage());
         }
+
+        System.out.println("Main thread is done :)");
     }
 }
