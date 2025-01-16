041db6edf1adc530a91ca288087b1f20c97f9974
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2025-01-16 23:33:34 +0100

    Add Serveur class to implement basic server functionality with client communication

---
+++ Examen/SOCKETS/Serveur.java
@@ -0,0 +1,40 @@
+package Examen.SOCKETS;
+
+import java.io.*;
+import java.net.*;
+
+public class Serveur {
+    private static final int PORT = 9876;
+
+    public static void main(String[] args) {
+        try{
+            ServerSocket SS = new ServerSocket(PORT);
+            System.out.println("Le Serveur en écoute sur le Port "+ PORT );
+            Socket S = SS.accept();
+            System.out.println("Un Client vient de se connecter !");
+            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+            
+            PrintWriter out = new PrintWriter(S.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(S.getInputStream()));
+
+            //Demander à l'utilisateur le nom du serveur
+            System.out.println("Saisir le nom du serveur : ");
+            String serveurNom = consoleInput.readLine();
+
+            //Recevoir deux messages de chez l'utilisateur
+            for (int i = 0; i < 2; i++) {
+                String message = in.readLine();
+                System.out.println("Message Reçu du client : "+ message);
+
+                String acquittement = serveurNom + "acquitement pour le message:  " + message;
+                out.println(acquittement);
+            }
+            S.close();
+            SS.close();
+        } catch (IOException e) {
+            System.err.println("Erreur du serveur : " + e.getMessage());
+        }
+
+        
+    }
+}
